12ebef07c3d12fda54a7553a301e110d091630e6
Branimir Kolev <kolevb780@gmail.com>
2024-04-10 12:23:53 +0300

    add an accuracy score

--- breast_cancer_prediction.py
+++ breast_cancer_prediction.py
@@ -1,20 +1,21 @@
 from sklearn.datasets import load_breast_cancer 
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
 
+
+#loading the datasete
 breast_cancer_data = load_breast_cancer()
 
+#spliting the dataset
 x_train, x_test, y_train, y_test = train_test_split(breast_cancer_data.data, breast_cancer_data.target, test_size=0.2, random_state=100)
 
-best_score=0
-for k in range(1,101):
-    print('K: ', k)   
-    classifier = KNeighborsClassifier(n_neighbors= k)
-    classifier.fit(x_train, y_train)
-    score = classifier.score(x_test, y_test)
-    if score>best_score:
-        best_score = score
-        best_k = k
-    print(score)
+#training with the split data
+classifier = KNeighborsClassifier(n_neighbors= 23)
+classifier.fit(x_train, y_train)
 
-print(best_k)
+#making an prediction
+y_predict = classifier.predict(x_test)
+
+#accuracy score
+print(100*accuracy_score(y_test, y_predict),'%')
